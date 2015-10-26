eba1f92b03e2da0f366a1d889406bb2d95d1f6e1
noprom <tyee.noprom@qq.com>
2015-10-26 12:10:43 +0800

    add update and delete

--- src/main/java/com/huntdreams/controller/MainController.java
+++ src/main/java/com/huntdreams/controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -49,13 +50,52 @@ public class MainController {
 
         // 数据库中添加一个用户
         //userRepository.save(userEntity);
-
-
         // 数据库中添加一个用户，并立即刷新
         userRepository.saveAndFlush(userEntity);
 
-
         // 返回重定向 到 /users 请求
+        return "redirect:/users";
+    }
+
+    // 查看用户详情
+    // @PathVariable可以收集url中的变量，需匹配的变量用{}括起来
+    // 例如：访问 localhost:8080/showUser/1 ，将匹配 userId = 1
+    @RequestMapping(value = "/showUser/{userId}", method = RequestMethod.GET)
+    public String showUser(@PathVariable("userId") Integer userId, ModelMap modelMap) {
+        // 找到userId所表示的用户
+        UserEntity userEntity = userRepository.findOne(userId);
+
+        // 传递给请求页面
+        modelMap.addAttribute("user", userEntity);
+        return "userDetail";
+    }
+
+    // 更新用户信息 页面
+    @RequestMapping(value = "/updateUser/{userId}", method = RequestMethod.GET)
+    public String updateUser(@PathVariable("userId") Integer userId, ModelMap modelMap) {
+
+        // 找到userId所表示的用户
+        UserEntity userEntity = userRepository.findOne(userId);
+        // 传递给请求页面
+        modelMap.addAttribute("user", userEntity);
+        return "updateUser";
+    }
+
+    // 更新用户信息 操作
+    @RequestMapping(value = "/updateUserPost", method = RequestMethod.POST)
+    public String updateUserPost(@ModelAttribute("userP") UserEntity userEntity) {
+        // 更新用户信息
+        userRepository.updateUser(userEntity.getUsername(), userEntity.getPassword(), userEntity.getId());
+        return "redirect:/users";
+    }
+
+    // 删除用户
+    @RequestMapping(value = "/deleteUser/{userId}", method = RequestMethod.GET)
+    public String deleteUser(@PathVariable("userId") Integer userId) {
+        // 删除id为userId的用户
+        userRepository.delete(userId);
+        // 立即刷新
+        userRepository.flush();
         return "redirect:/users";
     }
 }
