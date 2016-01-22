0fca23ff601012360c5bf2316f2c118a2eeb414d
noprom <tyee.noprom@qq.com>
2016-01-22 12:18:19 +0800

    init

--- src/main/java/com/huntdreams/controller/MainController.java
+++ src/main/java/com/huntdreams/controller/MainController.java
@@ -1,101 +1,16 @@
 package com.huntdreams.controller;
 
-import com.huntdreams.model.UserEntity;
-import com.huntdreams.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
 
 /**
  * Created by noprom on 10/23/15.
  */
 @Controller
 public class MainController {
-
-    // 自动装配
-    @Autowired
-    private UserRepository userRepository;
-
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "index";
-    }
-
-    // 用户管理
-    @RequestMapping(value = "/users", method = RequestMethod.GET)
-    public String users(ModelMap modelMap) {
-        // 找到user表里的所有记录
-        List<UserEntity> userList = userRepository.findAll();
-        // 将所有记录传递给返回的jsp页面
-        modelMap.addAttribute("userList", userList);
-        // 返回 pages 目录下的 userManage.jsp 页面
-        return "usermanage";
-    }
-
-    // 添加用户 页面
-    @RequestMapping(value = "/addUser", method = RequestMethod.GET)
-    public String addUser() {
-        return "addUser";
-    }
-
-    // 添加用户 处理
-    @RequestMapping(value = "/addUserPost", method = RequestMethod.POST)
-    public String addUserPost(@ModelAttribute("user") UserEntity userEntity) {
-
-        // 数据库中添加一个用户
-        //userRepository.save(userEntity);
-        // 数据库中添加一个用户，并立即刷新
-        userRepository.saveAndFlush(userEntity);
-
-        // 返回重定向 到 /users 请求
-        return "redirect:/users";
-    }
-
-    // 查看用户详情
-    // @PathVariable可以收集url中的变量，需匹配的变量用{}括起来
-    // 例如：访问 localhost:8080/showUser/1 ，将匹配 userId = 1
-    @RequestMapping(value = "/showUser/{userId}", method = RequestMethod.GET)
-    public String showUser(@PathVariable("userId") Integer userId, ModelMap modelMap) {
-        // 找到userId所表示的用户
-        UserEntity userEntity = userRepository.findOne(userId);
-
-        // 传递给请求页面
-        modelMap.addAttribute("user", userEntity);
-        return "userDetail";
-    }
-
-    // 更新用户信息 页面
-    @RequestMapping(value = "/updateUser/{userId}", method = RequestMethod.GET)
-    public String updateUser(@PathVariable("userId") Integer userId, ModelMap modelMap) {
-
-        // 找到userId所表示的用户
-        UserEntity userEntity = userRepository.findOne(userId);
-        // 传递给请求页面
-        modelMap.addAttribute("user", userEntity);
-        return "updateUser";
-    }
-
-    // 更新用户信息 操作
-    @RequestMapping(value = "/updateUserPost", method = RequestMethod.POST)
-    public String updateUserPost(@ModelAttribute("userP") UserEntity userEntity) {
-        // 更新用户信息
-        userRepository.updateUser(userEntity.getUsername(), userEntity.getPassword(), userEntity.getId());
-        return "redirect:/users";
-    }
-
-    // 删除用户
-    @RequestMapping(value = "/deleteUser/{userId}", method = RequestMethod.GET)
-    public String deleteUser(@PathVariable("userId") Integer userId) {
-        // 删除id为userId的用户
-        userRepository.delete(userId);
-        // 立即刷新
-        userRepository.flush();
-        return "redirect:/users";
     }
 }
