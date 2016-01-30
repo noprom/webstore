b668f71313d1ce154c35140df241e198b6aad0ff
noprom <tyee.noprom@qq.com>
2016-01-30 17:39:52 +0800

    add CartItem

---
+++ src/main/java/com/huntdreams/domain/CartItem.java
@@ -0,0 +1,77 @@
+package com.huntdreams.domain;
+
+import java.math.BigDecimal;
+
+/**
+ * 购物车类
+ * Created by noprom on 1/30/16.
+ */
+public class CartItem {
+
+    private Product product;    // 商品
+    private int quantity;       // 数量
+    private BigDecimal totalPrice;  // 总价
+    public CartItem() {
+
+    }
+
+    public CartItem(Product product) {
+        this.product = product;
+        this.quantity = 1;
+        this.totalPrice = product.getUnitPrice();
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+        this.updateTotalPrice();
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+        this.updateTotalPrice();
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    private void updateTotalPrice() {
+        totalPrice = this.product.getUnitPrice().multiply(new BigDecimal(this.quantity));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 311;
+        int result = 1;
+        result = prime * result + ((product == null) ? 0 : product.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj || obj == null || getClass() != obj.getClass()) {
+            return true;
+        }
+        CartItem other = (CartItem) obj;
+        if (product == null) {
+            if (other.product != null) {
+                return false;
+            }
+        } else if (!product.equals(other.product)) {
+            return false;
+        }
+        return true;
+    }
+}
