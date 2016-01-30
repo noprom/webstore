d022ccbf82eaabf3873ef29d7d3b715d3127431d
noprom <tyee.noprom@qq.com>
2016-01-30 22:59:36 +0800

    implemented OrderService

--- src/main/java/com/huntdreams/service/OrderService.java
+++ src/main/java/com/huntdreams/service/OrderService.java
@@ -1,8 +1,13 @@
 package com.huntdreams.service;
 
+import com.huntdreams.domain.Order;
+
 /**
  * Created by noprom on 1/22/16.
  */
 public interface OrderService {
     void processOrder(String productId, int count);
+//    public interface OrderRepository {
+//        Long saveOrder(Order order);
+//    }
 }
--- src/main/java/com/huntdreams/service/impl/OrderServiceImpl.java
+++ src/main/java/com/huntdreams/service/impl/OrderServiceImpl.java
@@ -1,7 +1,10 @@
 package com.huntdreams.service.impl;
 
+import com.huntdreams.domain.Order;
 import com.huntdreams.domain.Product;
 import com.huntdreams.domain.repository.ProductRepository;
+import com.huntdreams.domain.repository.OrderRepository;
+import com.huntdreams.service.CartService;
 import com.huntdreams.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,11 +18,33 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private CartService cartService;
+
+    /**
+     * 处理订单
+     * @param productId
+     * @param count
+     */
     public void processOrder(String productId, int count) {
         Product productById = productRepository.getProductById(productId);
         if (productById.getUnitsInStock() < count) {
             throw new IllegalArgumentException("Out of Stock. Available Units in stock"+ productById.getUnitsInStock());
         }
         productById.setUnitsInStock(productById.getUnitsInStock() - count);
+    }
+
+    /**
+     * 保存订单
+     * @param order
+     * @return
+     */
+    public Long saveOrder(Order order) {
+        Long orderId = orderRepository.saveOrder(order);
+        cartService.delete(order.getCart().getCartId());
+        return orderId;
     }
 }
