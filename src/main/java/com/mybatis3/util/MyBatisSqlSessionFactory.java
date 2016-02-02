c1decd2cb23cdff3c572a51698a253818e0a06ab
noprom <tyee.noprom@qq.com>
2016-02-02 23:24:47 +0800

    implemenyted sqlSessionFactory

---
+++ src/main/java/com/mybatis3/util/MyBatisSqlSessionFactory.java
@@ -0,0 +1,37 @@
+package com.mybatis3.util;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * This class is for instantiate and hold
+ * the SqlSessionFactory singleton object
+ * Created by noprom on 2/2/16.
+ */
+public class MyBatisSqlSessionFactory {
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+    public static SqlSessionFactory getSqlSessionFactory() {
+        if (sqlSessionFactory == null) {
+            InputStream inputStream;
+            try {
+                inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+                sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e.getCause());
+            }
+        }
+        return sqlSessionFactory;
+    }
+
+    public static SqlSession openSession() {
+        return getSqlSessionFactory().openSession();
+    }
+}
